4ff498cf4be53380856c889f85173a08308cdc68
Víctor Hernández <hernandezgarridovictor@gmail.com>
2025-09-13 11:48:03 +0200

    finish testing

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,4 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-
-const routes = require('./routes');
-const notFound = require('./middleware/notFound');
-const errorHandler = require('./middleware/errorHandler');
-
-const app = express();
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use('/api', routes);
-app.use(notFound);
-app.use(errorHandler);
+const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
